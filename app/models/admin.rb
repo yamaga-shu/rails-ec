30b40276bb8b9eb98445ef8aa413a3a720a2d7c9
shu.yamaga <s.yamaga.0318@gmail.com>
2025-11-22 17:44:56 +0900

    Update Admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,5 @@ class Admin < ApplicationRecord
   devise :authenticatable
 
   has_one :database_authentication, class_name: "Admin::DatabaseAuthentication"
+  has_one :registration, class_name: "Admin::Registration"
 end
